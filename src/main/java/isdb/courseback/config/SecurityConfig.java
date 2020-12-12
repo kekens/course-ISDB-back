cba3103e92b2a106757b19fe0f70c7784258b229
kirill-bobr <kirillbobryakov82@gmail.com>
2020-12-12 18:15:32 +0800

    Update SecurityConfig and create WebConfig for implementing CORS

--- src/main/java/isdb/courseback/config/SecurityConfig.java
+++ src/main/java/isdb/courseback/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/**")
+                .antMatchers("/api/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated();
