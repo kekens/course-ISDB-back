ba4be9ec9102a67a81a32d27f97fed8ebab65876
kirill-bobr <kirillbobryakov82@gmail.com>
2020-12-16 05:10:05 +0800

    Add function to select Auto Id

--- src/main/java/isdb/courseback/repository/DeliveryAutoRepository.java
+++ src/main/java/isdb/courseback/repository/DeliveryAutoRepository.java
@@ -6,11 +6,15 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface DeliveryAutoRepository extends CrudRepository<DeliveryAuto, Long> {
 
     @Query("SELECT minerId FROM DeliveryAuto")
     List<Integer> findAllMinerId();
+
+    @Query("SELECT autoId FROM DeliveryAuto WHERE autoId = :auId")
+    Optional<Integer> findAutoIdByMinerId(int auId);
 
 }
