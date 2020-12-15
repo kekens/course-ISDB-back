ff22bdf97469b9cdb5d0676e3503717441c392a3
kirill-bobr <kirillbobryakov82@gmail.com>
2020-12-16 05:06:26 +0800

    Create Auto, Equipment, AutoRepository, EquipmentRepository

---
+++ src/main/java/isdb/courseback/model/Equipment.java
@@ -0,0 +1,27 @@
+package isdb.courseback.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.repository.CrudRepository;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@Table(name = "equipment")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Equipment {
+
+    @Id
+    @Column(name = "equipment_id")
+    private int equipmentId;
+
+    @Column(name = "name")
+    private String name;
+
+}
