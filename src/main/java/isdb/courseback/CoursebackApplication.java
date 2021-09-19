258ab25a9a0bcef1033f468df61de8e7742c1587
kirill <kirillbobryakov82@gmail.com>
2021-09-20 00:52:35 +0300

    new auth init

--- src/main/java/isdb/courseback/CoursebackApplication.java
+++ src/main/java/isdb/courseback/CoursebackApplication.java
@@ -4,7 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class CoursebackApplication extends SpringBootServletInitializer {
 
@@ -12,9 +19,5 @@ public class CoursebackApplication extends SpringBootServletInitializer {
 		SpringApplication.run(CoursebackApplication.class, args);
 	}
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(CoursebackApplication.class);
-	}
 
 }
